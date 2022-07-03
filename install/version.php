0694bd0a164d99be2430a31d9ccd5b12525b5991
Danil Isaev <danil@isaev.digital>
2022-07-03 16:44:30 +0300

    Релиз 1.8.1

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.8.0",
-    "VERSION_DATE" => "2022-06-24 00:00:00"
+    "VERSION" => "1.8.1",
+    "VERSION_DATE" => "2022-07-03 00:00:00"
 );
